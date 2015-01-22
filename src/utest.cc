ffb7949af315019daf7de0569945378d87b4a0cd
Baker Bokorney <bbokorney@gmail.com>
2015-01-22 14:11:10 -0500

    Added DBFile useage.

--- src/utest.cc
+++ src/utest.cc
@@ -1,7 +1,13 @@
 #include <gtest/gtest.h>
+#include "DBFile.h"
 
 TEST(Test, Test) {
   ASSERT_EQ(true, true);
+}
+
+TEST(DBFile, Test) {
+  DBFile f = *(new DBFile());
+  ASSERT_EQ(0, f.Close());
 }
 
 int main(int argc, char** argv) {
