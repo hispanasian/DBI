4c35ef59cdc9887c2c72afbfdb6786cd922b6a78
Carlos Vasquez <hispanasian@gmail.com>
2015-01-25 02:32:07 -0500

    DBConfig::Read is implemented and has passed it's tests

--- src/DBConfig.cc
+++ src/DBConfig.cc
@@ -43,6 +43,26 @@ bool DBConfig::Write(RawFile &file) {
     file.Truncate();
     // Let's assume that file is at the beginning
     return false;
+    // Let's assume that file is at the beginning
+    // TODO: Update to deal with lseek so this assumption isn't made.
+    bool success = true;
+    std::string line;
+    std::string empty = "";
+    std::string key;
+    std::string val;
+    int delim;
+
+    while(success && empty.compare(line = file.ReadLine()) != 0) {
+        delim = line.find_first_of("=");
+        if(delim == string::npos) success = false;
+        else {
+            key = line.substr(0, delim);
+            val = line.substr(delim + 1, line.length() + 1);
+            map.insert(std::pair<std::string, std::string>(key, val));
+        }
+    }
+    success = file.Close() && success;
+    return success;
 }
 
 bool DBConfig::Write(RawFile &file) {
