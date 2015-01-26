4160c854914c5f7319fb1476a82c7a3e1c0e7a09
Carlos Vasquez <hispanasian@gmail.com>
2015-01-26 16:42:01 -0500

    Changed DBConfig to call file.LSeek(0) during read and write

--- src/DBConfig.cc
+++ src/DBConfig.cc
@@ -40,34 +40,6 @@ bool DBConfig::Write(RawFile &file) {
 
     success = file.Close() && success;
     return success;
-    file.Truncate();
-    // Let's assume that file is at the beginning
-
-    return false;
-    // Let's assume that file is at the beginning
-    // TODO: Update to deal with lseek so this assumption isn't made.
-    bool success = true;
-    std::string line;
-    std::string empty = "";
-    std::string key;
-    std::string val;
-    int delim;
-
-    while(success && empty.compare(line = file.ReadLine()) != 0) {
-        delim = line.find_first_of("=");
-        if(delim == string::npos) success = false;
-        else {
-            key = line.substr(0, delim);
-            val = line.substr(delim + 1, line.length() + 1);
-            map.insert(std::pair<std::string, std::string>(key, val));
-        }
-    }
-    success = file.Close() && success;
-    return success;
-}
-
-bool DBConfig::Write(RawFile &file) {
-    return false;
 }
 
 void DBConfig::AddKey(string key, string value) {
@@ -87,16 +59,6 @@ void DBConfig::AddKey(string key, string value) {
 
 std::string DBConfig::GetKey(string key) {
     string value;
-<<<<<<< HEAD
-    try {
-        value = map.at(key);
-    } catch(out_of_range e) {
-        value = "";
-    }
-    return value;
-=======
->>>>>>> Implemented and tested DBConfig::AddKey
-
     try {
         value = map.at(key);
     } catch(out_of_range e) {
@@ -104,4 +66,3 @@ std::string DBConfig::GetKey(string key) {
     }
     return value;
 }
-
