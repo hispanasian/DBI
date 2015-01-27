4b5392eebe9852eafa72ccec132ebeb8f5e8f56c
Carlos Vasquez <hispanasian@gmail.com>
2015-01-26 21:27:58 -0500

    Mocked DBFile, commented it's fucntions, and added some variables to it.

--- include/DBFile.h
+++ include/DBFile.h
@@ -13,21 +13,73 @@ typedef enum {heap, sorted, tree} fType;
 // stub DBFile header..replace it with your own DBFile.h 
 
 class DBFile {
-
+private:
+	off_t curPage;
+	File file;
 public:
 	DBFile ();
     virtual ~DBFile();
 
-	int Create (char *fpath, fType file_type, void *startup);
-	int Open (char *fpath);
-	int Close ();
+    /**
+     * Creates a file at fpath of file_type heap, sorted, or tree. It will also create a file to
+     * store meta-data located at filepath.header.
+     * @param fpath		The path to the file that should be created.
+     * @param file_type	The type of the file as defined by ftype
+     * @param startup	Unknown
+     * @return			1 if successful and 0 if failure.
+     */
+	virtual int Create (char *fpath, fType file_type, void *startup);
 
-	void Load (Schema &myschema, char *loadpath);
+	/**
+	 * Opens the file and associated header located at fpath and fpath.header respectivel. This
+	 * function does not load any data from the file into memory.
+	 * @param fpath	The path to the DBFile
+	 * @return 		1 if successful and 0 if failure.
+	 */
+	virtual int Open (char *fpath);
 
-	void MoveFirst ();
-	void Add (Record &addme);
-	int GetNext (Record &fetchme);
-	int GetNext (Record &fetchme, CNF &cnf, Record &literal);
+	/**
+	 * Closes the file held by this object.
+	 * @return 1 if successful and 0 if failure.
+	 */
+	virtual int Close ();
+
+	/**
+	 * Appends the data located at loadpath to memory.
+	 * @param myschema	The schema of the file being loaded to memory
+	 * @param loadpath	The path to the file being loaded
+	 */
+	virtual void Load (Schema &myschema, char *loadpath);
+
+	/**
+	 * Moves the pointer from the current record to the next record.
+	 */
+	virtual void MoveFirst ();
+
+	/**
+	 * Adds addme to a file and consumes addme.
+	 * @param addme	The Record being added to DBFile
+	 */
+	virtual void Add (Record &addme);
+
+	/**
+	 * Puts the current record in fetchme and moves the pointer to the next record. Upon use, the
+	 * record referenced by fetchme is consumed and the new record in fetchme is removed from DB
+	 * File.
+	 * @param fetchme	A reference that will be used to return the current Record
+	 * @return			0 if there is no valid record to be returned.
+	 */
+	virtual int GetNext (Record &fetchme);
+
+	/**
+	 * Looks for the next record that passes applyMe when applied between a Record in DBFile and
+	 * literal. This function increments the pointer accordingly.
+	 * @param fetchme	A reference that will be used to return the current Record
+	 * @param applyMe	The CNF that will be applied between the Record(s) in DBFile and literal
+	 * @param literal	The Record that will be compared with the Records in DBFile.
+	 * @return			0 if there is no valid record to be returned.
+	 */
+	virtual int GetNext (Record &fetchme, CNF &cnf, Record &literal);
 
 };
 #endif
