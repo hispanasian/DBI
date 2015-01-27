771af729167965243cf2ab942cc4b68b966c2d57
Carlos Vasquez <hispanasian@gmail.com>
2015-01-26 22:54:46 -0500

    Added DBFileTest.cc

--- src/DBFile.cc
+++ src/DBFile.cc
@@ -7,6 +7,9 @@
 #include "DBFile.h"
 #include "Defs.h"
 #include <iostream>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdio.h>
 
 // stub file .. replace it with your own DBFile.cc
 
@@ -19,6 +22,7 @@ DBFile::~DBFile () {
 }
 
 int DBFile::Create (char *f_path, fType f_type, void *startup) {
+
 }
 
 void DBFile::Load (Schema &f_schema, char *loadpath) {
@@ -43,4 +47,15 @@ int DBFile::GetNext (Record &fetchme) {
 }
 
 int DBFile::GetNext (Record &fetchme, CNF &cnf, Record &literal) {
+}
+
+/**
+ * Checks if a file located at path exists. This code was stolen from:
+ * http://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exist-using-standard-c-c11-c
+ * @param name	The name of the file
+ * @return True if the file exists
+ */
+inline bool FileExists(const std::string& file) {
+  struct stat buffer;
+  return (stat (file.c_str(), &buffer) == 0);
 }
---
+++ test/DBFileTest.cc
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "MockClasses.h"
+
+using ::testing::_;
