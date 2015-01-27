623d67c0253dee63d99ff579bc4f09828fd9d242
Baker Bokorney <bbokorney@gmail.com>
2015-01-27 00:38:44 -0500

    Removed unused namespace.

--- test/RawFileTest.cc
+++ test/RawFileTest.cc
@@ -1,9 +1,8 @@
 #include <gtest/gtest.h>
-#include "RawFile.h"
 #include <iostream>
-
-using ::testing::_;
+#include "RawFile.h"
 
 TEST(RawFile, OpenAndClose) {
+    ASSERT_EQ(0,0);
     std::cout << "sup there" << std::endl;
 }
