a31d5eac8e3de37067e090cd05de9780a059067a
Baker Bokorney <bbokorney@gmail.com>
2015-01-26 23:24:31 -0500

    RawFile implemented, needs testing.

--- include/RawFile.h
+++ include/RawFile.h
@@ -2,9 +2,14 @@
 #define RAWFILE_H
 
 #include <unistd.h>
+#include <stdio.h>
 #include <string>
 
 class RawFile {
+private:
+	FILE* file;
+	char buffer[4096];
+	std::string filename;
 public:
     RawFile();
     virtual ~RawFile();
@@ -12,7 +17,7 @@ public:
     virtual bool Close();
     virtual int Read(void* buf, size_t count);
     virtual int Write(const void* buf, size_t count);
-    virtual std::string ReadLine();
+    virtual bool ReadLine(std::string* line);
     virtual bool Append(std::string value);
     virtual bool Truncate();
     virtual void LSeek(off_t offset);
