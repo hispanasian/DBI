fb868aa4668a1f91aa64778c3a8bbdf598215775
Carlos Vasquez <hispanasian@gmail.com>
2015-01-27 02:52:55 -0500

    Getting the test cases started

--- test/DBFileTest.cc
+++ test/DBFileTest.cc
@@ -19,5 +19,14 @@ off_t DBFileTest::CurPage() { return file.curPage; }
 
 File DBFileTest::GetFile() { return file.file; }
 
+/**
+ * DBFile:Create should check of a file called "asdasdasd" exists (and it should not). it should
+ * then call Create on file and leave it's curPage unchanged. It should also create a header called
+ * "asdasdasd.header"
+ */
 TEST_F(DBFileTest, Create1) {
+	EXPECT_CALL(mock, Create("asdasdasd"));
+
+	// Remove the file called "asdasdasd" and "asdasdasd.header"
+	FILE
 }
