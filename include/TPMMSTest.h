4df9bb0ec96958847cdc852cf92f47ff43f3ff01
Carlos Vasquez <hispanasian@gmail.com>
2015-02-12 08:39:32 -0500

    Added TPMMSTest.h which contains the TPMMSTest fixture

---
+++ include/TPMMSTest.h
@@ -0,0 +1,43 @@
+#ifndef INCLUDE_TPMMSTEST_H_
+#define INCLUDE_TPMMSTEST_H_
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include "MockClasses.h"
+#include "BigQ.h"
+
+using ::testing::_;
+using ::testing::Return;
+using ::testing::InSequence;
+using ::testing::Sequence;
+using ::testing::StrictMock;
+using ::testing::AtMost;
+using ::testing::AtLeast;
+using ::testing::ByRef;
+using ::testing::Pointee;
+using ::testing::NotNull;
+using ::testing::SetArgPointee;
+using ::testing::DoAll;
+using ::testing::Eq;
+using ::testing::Ref;
+
+class TPMMSTest: public ::testing::Test {
+public:
+	StrictMock<MockPipe> in;
+	StrictMock<MockPipe> out;
+	StrictMock<MockFile> file;
+	StrictMock<MockPage> page;
+	StrictMock<MockRecord> *rec = new StrictMock<MockRecord>;
+	StrictMock<ComparisonEngine> comp;
+	OrderMaker order;
+	vector<int> runPos;
+	vector<Record *> run;
+	int runlen = 3;
+
+	TPMMS tpmms = TPMMS(in, out, file, page, comp, order, runPos, run, runlen);
+};
+
+#endif /* INCLUDE_TPMMSTEST_H_ */
