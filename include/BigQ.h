08989c02a7f2a7af889c2c5bc8585f1954c4294c
Carlos Vasquez <hispanasian@gmail.com>
2015-02-12 02:07:57 -0500

    Added some basic functions to BigQ

--- include/BigQ.h
+++ include/BigQ.h
@@ -2,6 +2,7 @@
 #define BIGQ_H
 #include <pthread.h>
 #include <iostream>
+#include <vector>
 #include "Pipe.h"
 #include "File.h"
 #include "Record.h"
@@ -15,5 +16,75 @@ public:
 	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
 	~BigQ ();
 };
+
+struct TPPMSData {
+
+};
+
+/**
+ * Two-Phase Multiway merge Sort
+ */
+void TPMMS();
+
+/**
+ * Phase 1 of the TPMMS algorithm. This algorithm will take records from in and, once runlen
+ * number of pages have been filled (or there are no more pages), sort the records and write them
+ * to file. It will repeat this process until there are no more records (in has been shut down).
+ * This will return the number of runs that have been generated. Note: This particular Phase1
+ * function contains the definition for Phase1, but this is meant for testing. Please use the
+ * Phase1 function with fewer arguments.
+ * @param in		The input Pipe
+ * @param out		The output Pipe
+ * @param order		The ordering definition
+ * @param file		The file that will store the sorted Pages
+ * @param runPages	A vector that will keep track of the number of pages in each run
+ * @param runlen	The number of Pages in a run
+ * #return			The number of runs that have been produced
+ */
+int Phase1(Pipe &in, Pipe &out, OrderMaker &order, File &file, vector<int> &runPages, const int &runlen);
+
+/**
+ * Phase 1 of the TPMMS algorithm. This algorithm will take records from in and, once runlen
+ * number of pages have been filled (or there are no more pages), sort the records and write them
+ * to file. It will repeat this process until there are no more records (in has been shut down).
+ * This will return the number of runs that have been generated. Note: This particular Phase1
+ * function contains the definition for Phase1, but this is meant for testing. Please use the
+ * Phase1 function with fewer arguments.
+ * @param in		The input Pipe
+ * @param out		The output Pipe
+ * @param order		The ordering definition
+ * @param comp		The engine that will be used to compare Records
+ * @param file		The file that will store the sorted Pages
+ * @param runPages	A vector that will keep track of the number of pages in each run
+ * @param psge		A page that will temporarily hold the records
+ * @param runlen	The number of Pages in a run
+ * @return			The number of runs that have been produced
+ */
+int Phase1(Pipe &in, Pipe &out, OrderMaker &order, ComparisonEngine &comp, File &file,
+		vector<int> &runPages,  Page &page, const int &runlen);
+
+/**
+ * Phase 2 of the TPMMS algorithm. This phase will take the runs from disk and merge them into
+ * sorted pages that will be put into out.
+ * @param out		The output Pipe
+ * @param order		The ordering definition
+ * @param file		The file that will store the sorted Pages
+ * @param runPages	A vector that will keep track of the number of pages in each run
+ * @param runcount	The number of runs
+ */
+void Phase2(Pipe &out, OrderMaker &order, File &file, vector<int> &runPages, const int &runcount);
+
+/**
+ * Phase 2 of the TPMMS algorithm. This phase will take the runs from disk and merge them into
+ * sorted pages that will be put into out. This particular Phase2 function contains the definition
+ * for Phase2, but this is meant for testing. Please use the Phase2 function with fewer arguments.
+ * @param out		The output Pipe
+ * @param order		The ordering definition
+ * @param file		The file that will store the sorted Pages
+ * @param runPages	A vector that will keep track of the number of pages in each run
+ * @param runs		A vector that keeps the "smallest" pages of each run in memory.
+ * @param runcount	The number of runs
+ */
+void Phase2(Pipe &out, OrderMaker &order, File &file, vector<int> &runPages, vector<Page*> &runs, const int &runcount);
 
 #endif
--- src/BigQ.cc
+++ src/BigQ.cc
@@ -12,3 +12,20 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 
 BigQ::~BigQ () {
 }
+
+int Phase1(Pipe &in, Pipe &out, OrderMaker &order, File &file, vector<int> &runPages, const int &runlen) {
+	return 0;
+}
+
+int Phase1(Pipe &in, Pipe &out, OrderMaker &order, ComparisonEngine &comp, File &file,
+		vector<int> &runPages,  Page &page, const int &runlen) {
+	return 0;
+}
+
+void Phase2(Pipe &out, OrderMaker &order, File &file, vector<int> &runPages, const int &runcount) {
+
+}
+
+void Phase2(Pipe &out, OrderMaker &order, File &file, vector<int> &runPages, vector<Page*> &runs, const int &runcount) {
+
+}
