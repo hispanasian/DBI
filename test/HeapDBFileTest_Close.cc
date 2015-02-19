82c9214ed43df9601791ced81f01d5d3e160f279
Carlos Vasquez <hispanasian@gmail.com>
2015-02-19 07:03:28 -0500

    Refactored HeapDBFileTest_Close

--- test/HeapDBFileTest_Close.cc
+++ test/HeapDBFileTest_Close.cc
@@ -5,38 +5,48 @@
  * and cursorIndex. It should then return 1.
  */
 TEST_F(HeapDBFileTest, Close1) {
+	SetDBToheapdb();
 	SetCursorIndex(5);
 	SetLastIndex(6);
-	SetCursor(cursor);
-	SetLast(last);
+
+	MockPage *cursor = new StrictMock<MockPage>;
+	MockPage *last = new StrictMock<MockPage>;
+	SetCursorNull();
+	SetLastNull();
+	SetCursorP(cursor);
+	SetLastP(last);
+	MockPage &lastRef = *last;
 
 	Sequence s1, s2;
 	EXPECT_CALL(config, Write(Ref(rfile))).
 			InSequence(s1, s2).
 			WillOnce(Return(true));
 	EXPECT_CALL(config, Clear()).
+			Times(AtLeast(1)).
 			InSequence(s1);
 	EXPECT_CALL(rfile, Close()).
 			InSequence(s2).
 			WillOnce(Return(true));
 
 	Sequence s3;
-	EXPECT_CALL(mockFile, AddPage(&last, 6)).
+	EXPECT_CALL(mockFile, AddPage(&lastRef, 6)).
 			InSequence(s3);
-	EXPECT_CALL(last, EmptyItOut()).
+	EXPECT_CALL(*last, EmptyItOut()).
 			InSequence(s3);
 
 	// Order doesn't matter
-	EXPECT_CALL(cursor, EmptyItOut());
+	EXPECT_CALL(*cursor, EmptyItOut());
 	EXPECT_CALL(mockFile, Close());
 
 
 	EXPECT_EQ(1, file.Close());
 	EXPECT_EQ(0, CursorIndex());
 	EXPECT_EQ(0, LastIndex());
+	EXPECT_EQ(NULL, Delegate());
 
 	SetCursorNull();
 	SetLastNull();
+	SetDBNull();
 }
 
 /**
@@ -44,29 +54,37 @@ TEST_F(HeapDBFileTest, Close1) {
  * clear config, zero out the indexes, and write out last.
  */
 TEST_F(HeapDBFileTest, Close2) {
+	SetDBToheapdb();
 	SetCursorIndex(5);
 	SetLastIndex(6);
-	SetCursor(cursor);
-	SetLast(last);
+
+	MockPage *cursor = new StrictMock<MockPage>;
+	MockPage *last = new StrictMock<MockPage>;
+	SetCursorNull();
+	SetLastNull();
+	SetCursorP(cursor);
+	SetLastP(last);
+	MockPage &lastRef = *last;
 
 	Sequence s1, s2;
 	EXPECT_CALL(config, Write(Ref(rfile))).
 			InSequence(s1, s2).
 			WillOnce(Return(false));
 	EXPECT_CALL(config, Clear()).
+			Times(AtLeast(1)).
 			InSequence(s1);
 	EXPECT_CALL(rfile, Close()).
 			InSequence(s2).
 			WillOnce(Return(true));
 
 	Sequence s3;
-	EXPECT_CALL(mockFile, AddPage(&last, 6)).
+	EXPECT_CALL(mockFile, AddPage(&lastRef, 6)).
 			InSequence(s3);
-	EXPECT_CALL(last, EmptyItOut()).
+	EXPECT_CALL(*last, EmptyItOut()).
 			InSequence(s3);
 
 	// Order doesn't matter
-	EXPECT_CALL(cursor, EmptyItOut());
+	EXPECT_CALL(*cursor, EmptyItOut());
 	EXPECT_CALL(mockFile, Close());
 
 	EXPECT_EQ(0, file.Close());
@@ -75,6 +93,7 @@ TEST_F(HeapDBFileTest, Close2) {
 
 	SetCursorNull();
 	SetLastNull();
+	SetDBNull();
 }
 
 /**
@@ -83,29 +102,37 @@ TEST_F(HeapDBFileTest, Close2) {
  * page and zero out the indexes.
  */
 TEST_F(HeapDBFileTest, Close4) {
+	SetDBToheapdb();
 	SetCursorIndex(5);
 	SetLastIndex(6);
-	SetCursor(cursor);
-	SetLast(last);
+
+	MockPage *cursor = new StrictMock<MockPage>;
+	MockPage *last = new StrictMock<MockPage>;
+	SetCursorNull();
+	SetLastNull();
+	SetCursorP(cursor);
+	SetLastP(last);
+	MockPage &lastRef = *last;
 
 	Sequence s1, s2;
 	EXPECT_CALL(config, Write(Ref(rfile))).
 			InSequence(s1, s2).
 			WillOnce(Return(true));
 	EXPECT_CALL(config, Clear()).
+			Times(AtLeast(1)).
 			InSequence(s1);
 	EXPECT_CALL(rfile, Close()).
 			InSequence(s2).
 			WillOnce(Return(false));
 
 	Sequence s3;
-	EXPECT_CALL(mockFile, AddPage(&last, 6)).
+	EXPECT_CALL(mockFile, AddPage(&lastRef, 6)).
 			InSequence(s3);
-	EXPECT_CALL(last, EmptyItOut()).
+	EXPECT_CALL(*last, EmptyItOut()).
 			InSequence(s3);
 
 	// Order doesn't matter
-	EXPECT_CALL(cursor, EmptyItOut());
+	EXPECT_CALL(*cursor, EmptyItOut());
 	EXPECT_CALL(mockFile, Close());
 
 	EXPECT_EQ(0, file.Close());
@@ -114,6 +141,7 @@ TEST_F(HeapDBFileTest, Close4) {
 
 	SetCursorNull();
 	SetLastNull();
+	SetDBNull();
 }
 
 /**
@@ -121,29 +149,37 @@ TEST_F(HeapDBFileTest, Close4) {
  * pages).
  */
 TEST_F(HeapDBFileTest, Close5) {
+	SetDBToheapdb();
 	SetCursorIndex(0);
 	SetLastIndex(0);
-	SetCursor(cursor);
-	SetLast(last);
+
+	MockPage *cursor = new StrictMock<MockPage>;
+	MockPage *last = new StrictMock<MockPage>;
+	SetCursorNull();
+	SetLastNull();
+	SetCursorP(cursor);
+	SetLastP(last);
+	MockPage &lastRef = *last;
 
 	Sequence s1, s2;
 	EXPECT_CALL(config, Write(Ref(rfile))).
 			InSequence(s1, s2).
 			WillOnce(Return(true));
 	EXPECT_CALL(config, Clear()).
+			Times(AtLeast(1)).
 			InSequence(s1);
 	EXPECT_CALL(rfile, Close()).
 			InSequence(s2).
 			WillOnce(Return(true));
 
 	Sequence s3;
-	EXPECT_CALL(mockFile, AddPage(&last, 0)).
+	EXPECT_CALL(mockFile, AddPage(&lastRef, 0)).
 			InSequence(s3);
-	EXPECT_CALL(last, EmptyItOut()).
+	EXPECT_CALL(*last, EmptyItOut()).
 			InSequence(s3);
 
 	// Order doesn't matter
-	EXPECT_CALL(cursor, EmptyItOut());
+	EXPECT_CALL(*cursor, EmptyItOut());
 	EXPECT_CALL(mockFile, Close());
 
 	EXPECT_EQ(1, file.Close());
@@ -152,4 +188,5 @@ TEST_F(HeapDBFileTest, Close5) {
 
 	SetCursorNull();
 	SetLastNull();
+	SetDBNull();
 }
