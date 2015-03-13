21ae5ec48be3df03ac4c0cc943ea5473070beadc
Carlos Vasquez <hispanasian@gmail.com>
2015-03-13 00:36:58 -0400

    Began implementing the Project Operation

---
+++ src/Project.cc
@@ -0,0 +1,26 @@
+/*
+ * Project.cc
+ *
+ *  Created on: Mar 13, 2015
+ *      Author: cvasquez
+ */
+
+#include "Project.h"
+
+Project::Project() {/* Do nothing */}
+
+Project::~Project() {/* Do nothing */}
+
+void Project::Use_n_Pages (int n) {/* Do nothing */}
+
+void Project::Run (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) {
+
+}
+
+void Project::Work (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) {
+
+}
+
+void Project::Work (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput, Record &rec) {
+
+}
