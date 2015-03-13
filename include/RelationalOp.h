ef1ba496b3ca174ea8279491cb1b7d78753d7a95
Carlos Vasquez <hispanasian@gmail.com>
2015-03-12 21:00:14 -0400

    Implemented more things for RelationalOp so that it would compile

--- include/RelationalOp.h
+++ include/RelationalOp.h
@@ -8,19 +8,22 @@
 #ifndef INCLUDE_RELATIONALOP_H_
 #define INCLUDE_RELATIONALOP_H_
 
+#include <pthread.h>
+
 class RelationalOp {
 protected:
 	int thread_id;
 	pthread_t worker;
 
 public:
+	RelationalOp();
+	virtual ~RelationalOp();
+
 	// blocks the caller until the particular relational operator
 	// has run to completion
 	virtual void WaitUntilDone () = 0;
 	// tells how much internal memory the operation can use
 	virtual void Use_n_Pages (int n) = 0;
-
-	virtual ~RelationalOp();
 };
 
 #endif /* INCLUDE_RELATIONALOP_H_ */
