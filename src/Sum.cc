fa2da2cc014149f0cfaf2e66231cdba234f5dd90
Carlos Vasquez <hispanasian@gmail.com>
2015-03-18 22:03:00 -0400

    Created stubs for Sum

---
+++ src/Sum.cc
@@ -0,0 +1,26 @@
+/*
+ * Sum.cc
+ *
+ *  Created on: Mar 18, 2015
+ *      Author: cvasquez
+ */
+
+#include "Sum.h"
+
+Sum::Sum() {/* Do nothing */}
+
+Sum::~Sum() {/* Do nothing */}
+
+void Sum::Use_n_Pages(int n) {/* Do nothing */}
+
+void Sum::Run(Pipe &inPipe, Pipe &outPipe, Function &computeMe) {
+
+}
+
+void Sum::Work (Pipe &inPipe, Pipe &outPipe, Function &computeMe) {
+
+}
+
+void Sum::Work (Pipe &inPipe, Pipe &outPipe, Function &computeMe, Record &rec) {
+
+}
