1ce719f3e15e862c80680cae52d9fc17ed70f3be
Carlos Vasquez <hispanasian@gmail.com>
2015-03-19 10:45:19 -0400

    Relation is no longer pure virtual

--- include/Relation.h
+++ include/Relation.h
@@ -9,6 +9,7 @@
 #define INCLUDE_RELATION_H_
 
 #include "Defs.h"
+#include "Record.h"
 
 class Relation {
 protected:
@@ -20,14 +21,14 @@ public:
 	 * @param memLimit	The maximum amount of memory that will be given to the Records
 	 */
 	Relation(int memLimit = PAGE_SIZE);
-	virtual ~Relation() = 0;
+	virtual ~Relation() {}
 
 	/**
 	 * Adds rec to the relation and returns true if successful (typically if memLimit is exceeded)
 	 * @param rec		The Record that will be added to the Relation (this is consumed)
 	 * @param return	True if rec was successfully added
 	 */
-	virtual bool Add(Record *rec) = 0;
+	virtual bool Add(Record *rec) { return true; }
 
 	/**
 	 * Returns the next Record in the Relation and puts it in rec. Returns false once we have run
@@ -35,17 +36,17 @@ public:
 	 * @param rec	The Record that will be returned (this is consumed).
 	 * @return		False if no Records remain
 	 */
-	virtual bool GetNext(Record *rec) = 0;
+	virtual bool GetNext(Record *rec) { return true; }
 
 	/**
 	 * Returns the Relation to the beginning.
 	 */
-	virtual void Reset() = 0;
+	virtual void Reset() {}
 
 	/**
 	 * Deletes Records from the Relation.
 	 */
-	virtual void Clear() = 0;
+	virtual void Clear() {}
 };
 
 
