6f6f8c1d716231127b137e295b953f8bfeb942a2
Carlos Vasquez <hispanasian@gmail.com>
2015-03-19 21:37:52 -0400

    Fixed some join conflicts and some header issues

--- include/JoinRelation.h
+++ include/JoinRelation.h
@@ -9,6 +9,7 @@
 #define INCLUDE_JOINRELATION_H_
 
 #include "Relation.h"
+#include "Pipe.h"
 
 class JoinRelation: public Relation {
 friend class JoinRelationTest;
@@ -20,21 +21,14 @@ public:
 	virtual ~JoinRelation();
 
 	/**
-<<<<<<< HEAD
 	 * Adds rec to the relation
 	 * @param rec		The Record that will be added to the Relation (this is consumed)
 	 * @param return	true if the record was added with a in memory relation and false if it was
 	 * 					added to a file relation.
-=======
-	 * Adds rec to the relation and returns true if memLimit is not exceeded
-	 * @param rec		The Record that will be added to the Relation (this is consumed)
-	 * @param return	True if rec was successfully added
->>>>>>> 61d369c1023d4bd810ec630267e3be022042be30
 	 */
 	virtual bool Add(Record *rec);
 
 	/**
-<<<<<<< HEAD
 	 * Populates this relation with all the Records from in. This will return true if it was able
 	 * to keep all the Records in memory.
 	 * @param in	The source of the Records for this relation
@@ -43,8 +37,6 @@ public:
 	virtual bool Populate(Pipe &in);
 
 	/**
-=======
->>>>>>> 61d369c1023d4bd810ec630267e3be022042be30
 	 * Returns the next Record in the Relation and puts it in rec. Returns false once we have run
 	 * out of Records. rec should not be NULL and should be instantiated.
 	 * @param rec	The Record that will be returned (this is consumed).
