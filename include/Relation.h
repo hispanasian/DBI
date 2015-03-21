9f30e83cd4ba40a3f5639dfa9b78a473e6579a85
Baker Bokorney <bbokorney@gmail.com>
2015-03-21 16:56:46 -0400

    Implemented a function which had no implementation and was causing a build error.

--- include/Relation.h
+++ include/Relation.h
@@ -52,7 +52,7 @@ public:
 	 * Returns the amount of memory used by this Relation (in bytes).
 	 * @return	the amount of memory used by this Relation (in bytes).
 	 */
-	virtual int MemUsed();
+	int MemUsed() {return 0;}
 };
 
 
