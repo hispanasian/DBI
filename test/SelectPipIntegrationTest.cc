55a40ec87f8ade7563f8705dfeb3a100d1048a60
Baker Bokorney <bbokorney@gmail.com>
2015-03-21 16:38:25 -0400

    Added a header file for shared functions.

--- test/SelectPipIntegrationTest.cc
+++ test/SelectPipIntegrationTest.cc
@@ -1,32 +1,9 @@
-#include <gtest/gtest.h>
-#include "Schema.h"
-#include "Record.h"
-#include "Pipe.h"
+#include "RelationalOpIntTest.h"
 #include "SelectPipe.h"
 
-extern "C" {
-	int yyparse(void);   // defined in y.tab.c
-	int yyfuncparse(void);   // defined in yyfunc.tab.c
-	void init_lexical_parser (char *); // defined in lex.yy.c (from Lexer.l)
-	void close_lexical_parser (); // defined in lex.yy.c
-	void init_lexical_parser_func (char *); // defined in lex.yyfunc.c (from Lexerfunc.l)
-	void close_lexical_parser_func (); // defined in lex.yyfunc.c
-}
-
-extern struct AndList *final;
-
-void SetupCNF(char *input, Schema *left, CNF &cnf_pred, Record &literal) {
-	init_lexical_parser (input);
-  	if (yyparse() != 0) {
-		cout << " Error: can't parse your CNF " << input << endl;
-		exit (1);
-	}
-	cnf_pred.GrowFromParseTree (final, left, literal); // constructs CNF predicate
-	close_lexical_parser ();
-}
 
 // Simple test, 25 records expected
-TEST(SelectPipeIntegrationTest, Test1) {
+TEST_F(RelationalOpIntTest, SelectPipe) {
 	FILE *tableFile = fopen ("data/10M/lineitem.tbl", "r");
 	Record temp;
 	Schema schema ("data/catalog", "lineitem");
