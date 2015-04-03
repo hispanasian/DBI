0afc8e1227055d9e4d9da984df9928de5dfa5531
Carlos Vasquez <hispanasian@gmail.com>
2015-04-02 20:36:46 -0400

    Implemented and tested Statistics::Read

--- include/Statistics.h
+++ include/Statistics.h
@@ -25,6 +25,8 @@ protected:
 	std::unordered_map<std::string, StatPair> myRelations;
 
 	Statistics(std::unordered_map<std::string, StatPair> &_relations);
+	virtual void Read(char *fromWhere, RawFile &file);
+	virtual void Write(char *fromWhere, RawFile &file);
 public:
 	Statistics();
 	virtual ~Statistics();
@@ -35,6 +37,29 @@ public:
 	 * @param numTuples	The number of tuples in the relation
 	 */
 	virtual void AddRel(char *relName, int numTuples);
+
+	/**
+	 * De-serializes the contents from the file located at fromWhere and puts it into this object.
+	 * The format of the file should be as follows:
+	 *
+	 * Relations
+	 * rel1=numTuples
+	 * reln=numTuples
+	 * Attributes
+	 * rel1.att1=numDistinct
+	 * reln.attj=numDistinct
+	 *
+	 * This method will throw a out_of_range exception if there is a relation.attribute pair for
+	 * which the relation does not exist (it was not declared in the Relations section)
+	 * @param fromWhere	The file containing a serialized contents of a Statistics object
+	 */
+	virtual void Read(char *fromWhere);
+
+	/**
+	 * Serializes the contents of Statistics to the file located at fromWhere
+	 * @param fromWhere	The file that will hold the serialized contents of this object
+	 */
+	virtual void Write(char *fromWhere);
 
 	/**
 	 * Returns the number of tuples in the provided relName.
--- src/Statistics.cc
+++ src/Statistics.cc
@@ -8,6 +8,7 @@
 using namespace std;
 
 #include "Statistics.h"
+#include <iostream>
 
 Statistics::Statistics(): relations(myRelations) {
 
@@ -23,6 +24,55 @@ Statistics::~Statistics() {
 
 void Statistics::AddRel(char *relName, int numTuples) {
 	relations[relName].numTuples = numTuples;
+}
+
+void Statistics::Read(char *fromWhere) {
+	RawFile file;
+	Read(fromWhere, file);
+}
+
+void Statistics::Read(char *fromWhere, RawFile &file) {
+	string line;
+	string rel;
+	string val;
+	int equals;
+
+	file.Open(fromWhere);
+	file.ReadLine(&line); // The first line should be Relations
+
+	// First, read in the relations
+	while(file.ReadLine(&line) == true && line.compare("Attributes") != 0) {
+		equals = line.find_first_of("=");
+		if(equals != string::npos) { // ignore incorrectly formatted relations
+			rel = line.substr(0, equals);
+			val = line.substr(equals + 1, line.length() + 1);
+			relations[rel].numTuples = stoi(val);
+		}
+	}
+
+	string att;
+	int dot;
+	// Now read in the attributes
+	while(file.ReadLine(&line) == true) {
+		equals = line.find_first_of("=");
+		dot = line.find_first_of(".");
+		if(equals != string::npos && dot != string::npos) { // ignore incorrectly formatted attributes
+			rel = line.substr(0, dot);
+			att = line.substr(dot+1, equals - dot - 1);
+			val = line.substr(equals + 1, line.length() + 1);
+			relations.at(rel).atts[att] = stoi(val);
+		}
+	}
+
+	file.Close();
+}
+
+void Statistics::Write(char *fromWhere) {
+
+}
+
+void Statistics::Write(char *fromWhere, RawFile &file) {
+
 }
 
 int Statistics::NumTuples(char *relName) {
