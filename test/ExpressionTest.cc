373494edf5f6b5bbe061246472345a41476c794e
Carlos Vasquez <hispanasian@gmail.com>
2015-04-03 23:51:07 -0400

    Tested all the methods in the Expressions and fixed a bug in BinaryExpression

--- test/ExpressionTest.cc
+++ test/ExpressionTest.cc
@@ -86,6 +86,86 @@ TEST(BinaryExpressionTest, Combine4) {
 }
 
 /**
+ * Numerator should return the number of tuples in any of the relations contained by the
+ * expression
+ */
+TEST(BinaryExpressionTest, Numerator) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddRel("rel2", 50);
+	stat.AddAtt("rel1", "att1", 0);
+	stat.AddAtt("rel2", "att2", 0);
+
+	BinaryExpression exp1 = BinaryExpression(stat, "rel1", "att1", "rel2", "att2", EQUALS);
+
+	EXPECT_EQ(50, exp1.Numerator());
+}
+
+/**
+ * Denominator should return the number of attributes in any of the relations contained by the
+ * expression if the operator is EQUALS
+ */
+TEST(BinaryExpressionTest, Denominator1) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddRel("rel2", 50);
+	stat.AddAtt("rel1", "att1", 10);
+	stat.AddAtt("rel2", "att2", 10);
+
+	BinaryExpression exp1 = BinaryExpression(stat, "rel1", "att1", "rel2", "att2", EQUALS);
+
+	EXPECT_EQ(10, exp1.Denominator());
+}
+
+/**
+ * Denominator should return three if the operator is not EQUALS
+ */
+TEST(BinaryExpressionTest, Denominator2) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddRel("rel2", 50);
+	stat.AddAtt("rel1", "att1", 10);
+	stat.AddAtt("rel2", "att2", 10);
+
+	BinaryExpression exp1 = BinaryExpression(stat, "rel1", "att1", "rel2", "att2", LESS_THAN);
+
+	EXPECT_EQ(3, exp1.Denominator());
+}
+
+/**
+ * Tuples should return: (rel1*rel2)/(att) where rel = the number of tuples in the given relation
+ * and att = the number of attributes in the expression. This should be the result when the
+ * operator is EQUALS
+ */
+TEST(BinaryExpressionTest, Tuples1) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddRel("rel2", 100);
+	stat.AddAtt("rel1", "att1", 10);
+	stat.AddAtt("rel2", "att2", 10);
+
+	BinaryExpression exp1 = BinaryExpression(stat, "rel1", "att1", "rel2", "att2", EQUALS);
+
+	EXPECT_EQ(500, exp1.Tuples());
+}
+
+/**
+ * Tuples should return: (rel1*rel2)/3 where rel = the number of tuples in the given relation.
+ * This should be the result when the operator is not EQUALS
+ */
+TEST(BinaryExpressionTest, Tuples2) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddRel("rel2", 100);
+	stat.AddAtt("rel1", "att1", 10);
+	stat.AddAtt("rel2", "att2", 10);
+
+	BinaryExpression exp1 = BinaryExpression(stat, "rel1", "att1", "rel2", "att2", GREATER_THAN);
+
+	EXPECT_DOUBLE_EQ((50*100)/3.0, exp1.Tuples());
+}
+
+/**
  * UnaryExpression should not combine with BinaryExpression
  */
 TEST(UnaryExpressionTest, Combine1) {
@@ -188,4 +268,94 @@ TEST(UnaryExpressionTest, Combine6) {
 
 	EXPECT_EQ(true, exp1.Combine(exp2));
 	EXPECT_EQ(1, exp1.Count());
+}
+
+/**
+ * Numerator should return the number of tuples in any of the relations contained by the
+ * expression
+ */
+TEST(UnaryExpressionTest, Numerator) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddAtt("rel1", "att1", 0);
+
+	UnaryExpression exp1 = UnaryExpression(stat, "rel1", "att1", "4", EQUALS);
+
+	EXPECT_EQ(50, exp1.Numerator());
+}
+
+/**
+ * Denominator should return the number of attributes in any of the relations contained by the
+ * expression if the operator is EQUALS
+ */
+TEST(UnaryExpressionTest, Denominator1) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddAtt("rel1", "att1", 10);
+
+	UnaryExpression exp1 = UnaryExpression(stat, "rel1", "att1", "4", EQUALS);
+
+	EXPECT_EQ(10, exp1.Denominator());
+}
+
+/**
+ * Denominator should return three if the operator is not EQUALS
+ */
+TEST(UnaryExpressionTest, Denominator2) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddAtt("rel1", "att1", 10);
+
+	UnaryExpression exp1 = UnaryExpression(stat, "rel1", "att1", "4", GREATER_THAN);
+
+	EXPECT_EQ(3, exp1.Denominator());
+}
+
+/**
+ * Tuples should return: (rel*lit)/(att) where rel = the number of tuples in the given relation,
+ * lit = the number of literals used in the expression, and att = the number of attributes in the
+ * expression. This should be the result when the operator is EQUALS
+ */
+TEST(UnaryExpressionTest, Tuples1) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddAtt("rel1", "att1", 10);
+
+	UnaryExpression exp1 = UnaryExpression(stat, "rel1", "att1", "4", EQUALS);
+
+	EXPECT_EQ(5, exp1.Tuples());
+}
+
+/**
+ * Tuples should return: (rel)/3 where rel = the number of tuples in the given relation.
+ * This should be the result when the operator is not EQUALS
+ */
+TEST(UnaryExpressionTest, Tuples2) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddRel("rel2", 100);
+	stat.AddAtt("rel1", "att1", 10);
+	stat.AddAtt("rel2", "att2", 10);
+
+	UnaryExpression exp1 = UnaryExpression(stat, "rel1", "att1", "4", GREATER_THAN);
+
+	EXPECT_DOUBLE_EQ(50/3.0, exp1.Tuples());
+}
+
+/**
+ * Tuples should return: (rel*lit)/(att) where rel = the number of tuples in the given relation,
+ * lit = the number of literals used in the expression, and att = the number of attributes in the
+ * expression. This should be the result when the operator is EQUALS
+ */
+TEST(UnaryExpressionTest, Tuples3) {
+	Statistics stat;
+	stat.AddRel("rel1", 50);
+	stat.AddAtt("rel1", "att1", 3);
+
+	UnaryExpression exp1 = UnaryExpression(stat, "rel1", "att1", "4", EQUALS);
+	UnaryExpression exp2 = UnaryExpression(stat, "rel1", "att1", "2", EQUALS);
+
+	exp1.Combine(exp2);
+
+	EXPECT_DOUBLE_EQ((50*2)/3.0, exp1.Tuples());
 }
