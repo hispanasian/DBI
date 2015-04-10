3f8eff63d599eb232560d2bf98e7251f27b6b3b0
Baker Bokorney <bbokorney@gmail.com>
2015-04-10 17:28:20 -0400

    Cleaned up comments. Removed commented code.

--- test/StatisticsTest_ComputeNumTuples.cc
+++ test/StatisticsTest_ComputeNumTuples.cc
@@ -16,9 +16,10 @@ TEST_F(StatisticsTest, ComputeNumTuples1) {
 
 /**
  * Should compute the number of tuples according to the formula
- * 1 + 1 + 1 = 3
- * 1 * 1 * 1 = 1
- * 3 - 1/1 = 2
+ * sum -> 1 + 1 + 1 = 3
+ * denominator -> 1 * 1 * 1 = 1
+ * numerator -> 1
+ * result -> 3 - 1/1 = 2
  */
 TEST_F(StatisticsTest, ComputeNumTuples2) {
 	StrictMock<MockExpression> mock;
@@ -40,9 +41,10 @@ TEST_F(StatisticsTest, ComputeNumTuples2) {
 
 /**
  * Should compute the number of tuples according to the formula
- * 1 + 2 + 3 = 6
- * 3 * 2 * 1 = 6
- * 6 - 3/6 = 5.5
+ * sum -> 1 + 2 + 3 = 6
+ * denominator -> 3 * 2 * 1 = 6
+ * numerator -> 3
+ * result -> 6 - 3/6 = 5.5
  */
 TEST_F(StatisticsTest, ComputeNumTuples3) {
 	StrictMock<MockExpression> mock;
@@ -76,47 +78,3 @@ TEST_F(StatisticsTest, ComputeNumTuples3) {
 	
 	EXPECT_EQ(5.5, stat.ComputeNumTuples(exps));
 }
-
-
-/**
- * Should combine only the expressions
- * which will combine, regardless of their positions
- * in the list of expressions
- * Elements 2 and 4 will combine, the rest will not
- */
-// TEST_F(StatisticsTest, CombineExpressions4) {
-// 	StrictMock<MockExpression> mock1;
-// 	StrictMock<MockExpression> mock2;
-// 	StrictMock<MockExpression> mock3;
-// 	StrictMock<MockExpression> mock4;
-// 	StrictMock<MockExpression> mock5;
-// 	vector<Expression*> exps;
-// 	exps.push_back(&mock1);
-// 	exps.push_back(&mock2);
-// 	exps.push_back(&mock3);
-// 	exps.push_back(&mock4);
-// 	exps.push_back(&mock5);
-
-// 	Sequence s;
-// 	EXPECT_CALL(mock1, Combine(_)).
-// 		WillRepeatedly(Return(false));
-// 	EXPECT_CALL(mock2, Combine(_)).
-// 		InSequence(s).
-// 		WillOnce(Return(false));
-// 	EXPECT_CALL(mock2, Combine(_)).
-// 		InSequence(s).
-// 		WillOnce(Return(true));
-// 	EXPECT_CALL(mock2, Combine(_)).
-// 		InSequence(s).
-// 		WillOnce(Return(false));
-// 	EXPECT_CALL(mock3, Combine(_)).
-// 		WillRepeatedly(Return(false));
-// 	EXPECT_CALL(mock4, Combine(_)).
-// 		WillRepeatedly(Return(false));
-// 	EXPECT_CALL(mock5, Combine(_)).
-// 		WillRepeatedly(Return(false));
-
-// 	stat.CombineExpressions(exps);	
-// 	EXPECT_EQ(4, exps.size());
-// }
-
