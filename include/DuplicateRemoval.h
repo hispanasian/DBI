a2f11463a829ce8e2c2967d19eee018208705074
Carlos Vasquez <hispanasian@gmail.com>
2015-04-17 19:22:39 -0400

    Modified DuplicateRemoval to take two Schemas

--- include/DuplicateRemoval.h
+++ include/DuplicateRemoval.h
@@ -25,9 +25,18 @@ public:
 	 * Removes the duplicate Records from inPipe and puts the unique Records in outPipe
 	 * @param inPipe	The source of Records being filtered
 	 * @param outPipe	The destination for the Selected Records
-	 * @param mySchema
+	 * @param mySchema	The Schema of the Records in inPipe
 	 */
 	virtual void Run (Pipe &inPipe, Pipe &outPipe, Schema &mySchema);
+
+	/**
+	 * Removes the duplicate Records from inPipe and puts the unique Records in outPipe
+	 * @param inPipe		The source of Records being filtered
+	 * @param outPipe		The destination for the Selected Records
+	 * @param mySchema		The Schema of the Records in inPipe
+	 * @param duplicates	The Schema of the duplicates that we want to remove
+	 */
+	virtual void Run (Pipe &inPipe, Pipe &outPipe, Schema &mySchema, Schema &duplicates);
 
 	/**
 	 * Sets the number of pages to use in the BigQ for soring
@@ -39,6 +48,12 @@ public:
 	 * inPipe is Sorted (ie, use BigQ)
 	 */
 	virtual void Remove(Pipe &inPipe, Pipe &outPipe, Schema &mySchema);
+
+	/**
+	 * This is a single-threaded implementation of the Removal. Note that this method expects that
+	 * inPipe is Sorted (ie, use BigQ)
+	 */
+	virtual void Remove(Pipe &inPipe, Pipe &outPipe, Schema &mySchema, Schema &duplicates);
 
 	/**
 	 * This is where the logic of the Removal goes. This is meant for testing and the other Remove
@@ -56,6 +71,7 @@ struct DuplicateRemovalData {
 	Pipe &in;
 	Pipe &out;
 	Schema &schema;
+	Schema &duplicates;
 	DuplicateRemoval &op;
 };
 
