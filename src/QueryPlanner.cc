7de7488057f5cfc436a2668263408701e70f536f
Carlos Vasquez <hispanasian@gmail.com>
2015-04-19 15:46:16 -0400

    Implemebted stubs for QueryPlanner

---
+++ src/QueryPlanner.cc
@@ -0,0 +1,22 @@
+/*
+ * QueryPlanner.cc
+ *
+ *  Created on: Apr 19, 2015
+ *      Author: cvasquez
+ */
+
+#include "QueryPlanner.h"
+
+QueryPlanner::QueryPlanner(): plan(NULL) {}
+
+QueryPlanner::~QueryPlanner() {
+	// TODO Auto-generated destructor stub
+}
+
+ExecutionPlan* QueryPlanner::GetPlan() const {
+	return plan;
+}
+
+void QueryPlanner::Plan(const SQL &sql, const RelFileMap &fileMap) {
+
+}
