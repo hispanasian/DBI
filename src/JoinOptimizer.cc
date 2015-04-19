68954689c6e331ea87dce2bf239f587a2a2cf3f1
Baker Bokorney <bbokorney@gmail.com>
2015-04-19 12:13:53 -0400

    Added some missing params to the signature of JoinOptimizer::Optimize() and elaborated the usage of this method with comments in the header file.

--- src/JoinOptimizer.cc
+++ src/JoinOptimizer.cc
@@ -2,4 +2,6 @@
 
 void JoinOptimizer::Optimize(unordered_map<string, AndList*> &selects,
                         unordered_map<string, unordered_map<string, AndList*> > &joins,
+                        Statistics& stats,
+                        vector<string> &rels,
                         vector<TupleCount> &counts) { }
