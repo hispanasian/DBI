e2e3f3e1d8cf8ea14132d6a2027418fae26bca4d
Baker Bokorney <bbokorney@gmail.com>
2015-04-30 17:42:06 -0400

    SelectPipe execution implemented.

--- src/ExecutionVisitor.cc
+++ src/ExecutionVisitor.cc
@@ -6,19 +6,21 @@ ExecutionVisitor::~ExecutionVisitor(){}
 void ExecutionVisitor::VisitSelectPipeNode(SelectPipeNode *node, void* arg){
 	node->child->Visit(*this, arg);
 	ExecutionVisitorData* data = (ExecutionVisitorData*) arg;
-
+	Pipe* pipe = new Pipe();
+	data->pipes.emplace(node->GetID(), pipe);
+	node->op.Run(*data->pipes.at(node->child->GetID()), *pipe, node->cnf, node->literal);
 }
+
 void ExecutionVisitor::VisitSelectFileNode(SelectFileNode *node, void* arg){
 	ExecutionVisitorData* data = (ExecutionVisitorData*) arg;
 	DBFile* dbfile = new DBFile(); 
 	int ret = dbfile->Open(node->fname.c_str());
-	cout << node->fname << endl;
-	cout << ret << endl;
 	data->files.push_back(dbfile);
 	Pipe* pipe = new Pipe();
 	data->pipes.emplace(node->GetID(), pipe);
 	node->op.Run(*dbfile, *pipe, node->cnf, node->literal);
 }
+
 void ExecutionVisitor::VisitProjectNode(ProjectNode *node, void* arg){
 	node->child->Visit(*this, arg);
 	ExecutionVisitorData* data = (ExecutionVisitorData*) arg;
@@ -27,6 +29,7 @@ void ExecutionVisitor::VisitProjectNode(ProjectNode *node, void* arg){
 	node->op.Run(*data->pipes.at(node->child->GetID()), *pipe, node->keepMe,
 		node->numAttsInput, node->numAttsOutput);
 }
+
 void ExecutionVisitor::VisitJoinNode(JoinNode *node, void* arg){}
 void ExecutionVisitor::VisitDuplicateRemovalNode(DuplicateRemovalNode *node, void* arg){
 	node->child->Visit(*this, arg);
@@ -35,12 +38,11 @@ void ExecutionVisitor::VisitDuplicateRemovalNode(DuplicateRemovalNode *node, voi
 	data->pipes.emplace(node->GetID(), pipe);
 	Schema& copy = *new Schema(*node->GetSchema());
 	Schema& duplicates = *new Schema(copy, node->duplicates);
-	// Schema& childCopy = *new Schema(*node->child->GetSchema());
 	cout << copy.ToString() << endl;
 	cout << duplicates.ToString() << endl;
 	node->op.Run(*data->pipes.at(node->child->GetID()), *pipe, copy, duplicates);
-	// node->op.Run(*data->pipes.at(node->child->GetID()), *pipe, childCopy, copy);
 }
+
 void ExecutionVisitor::VisitSumNode(SumNode *node, void* arg){
 	node->child->Visit(*this, arg);
 	ExecutionVisitorData* data = (ExecutionVisitorData*) arg;
@@ -48,6 +50,7 @@ void ExecutionVisitor::VisitSumNode(SumNode *node, void* arg){
 	data->pipes.emplace(node->GetID(), pipe);
 	node->op.Run(*data->pipes.at(node->child->GetID()), *pipe, node->function);
 }
+
 void ExecutionVisitor::VisitGroupByNode(GroupByNode *node, void* arg){}
 void ExecutionVisitor::VisitWriteOutNode(WriteOutNode *node, void* arg){
 	node->child->Visit(*this, arg);
