2083aae22f5153c0fef4cae7a1ab7f4612b8c166
Baker Bokorney <bbokorney@gmail.com>
2015-04-30 13:16:09 -0400

    Added some hard coded bootstrap.

--- src/drivertest.cc
+++ src/drivertest.cc
@@ -4,17 +4,30 @@
 
 using namespace std;
 
+void setupRelation(char* rel, char* catalogFName, const string& dbLocation,
+	RelationData& relations, Statistics& stats)  {
+	// first load the schema from the file
+	Schema* s = new Schema(catalogFName, rel);
+	// now use this to populate relations and stats
+	relations.Insert(string(rel), dbLocation, string(catalogFName), *s);
+	stats.AddRel(rel, 100);
+	for(int i = 0; i < s->GetNumAtts(); ++i) {
+		stats.AddAtt(rel, s->GetAtts()[i].name.c_str(), -1);
+	}
+}
+
+
 int main(int argc, char const *argv[])
 {
+	char* catalogFName = "data/catalog";	
+	string dbLocation = "data/DB/10M/";
 	Statistics stats = Statistics();
-	// stats.AddRel("nation", 25);
-	// stats.AddAtt("nation", "n_nationkey", -1);
-	// stats.AddAtt("nation", "n_name", -1);
-	// stats.AddAtt("nation", "n_regionkey", -1);
-	// stats.AddAtt("nation", "n_comment", -1);
-
 	RelationData relations = RelationData();
-	SQLEngine engine = SQLEngine(stats, relations, "data/DB/10M/", "catalog");
+
+	// add some relations
+	setupRelation("nation", catalogFName, dbLocation, relations, stats);
+
+	SQLEngine engine = SQLEngine(stats, relations, dbLocation, string(catalogFName));
 
 	Console console = Console(engine);
 	console.Start();
