8e8e353cd4d0c5c25db8e8f08590a4677c910e06
Baker Bokorney <bbokorney@gmail.com>
2015-04-30 20:25:08 -0400

    Fixed a bug I introduced.

--- src/WriteOut.cc
+++ src/WriteOut.cc
@@ -33,6 +33,7 @@ void WriteOut::Write(Pipe &inPipe, FILE *outFile, Schema &mySchema) {
 }
 
 void WriteOut::Write(Pipe &inPipe, FILE *outFile, Schema &mySchema, RawFile &file, Record &rec) {
+	file.Open(outFile);
 	while(inPipe.Remove(&rec)) {
 		file.Append(rec.ToString(&mySchema));
 	}
