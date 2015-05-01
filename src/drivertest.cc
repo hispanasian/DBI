85e7604d332c59dcd6d0dae1e816b230a37a5f43
Baker Bokorney <bbokorney@gmail.com>
2015-05-01 00:43:46 -0400

    Changed the program to use the actual statistics.

--- src/drivertest.cc
+++ src/drivertest.cc
@@ -10,10 +10,10 @@ void setupRelation(char* rel, char* catalogFName, const string& dbLocation,
 	Schema* s = new Schema(catalogFName, rel);
 	// now use this to populate relations and stats
 	relations.Insert(string(rel), dbLocation, string(catalogFName), *s);
-	stats.AddRel(rel, 100);
-	for(int i = 0; i < s->GetNumAtts(); ++i) {
-		stats.AddAtt(rel, s->GetAtts()[i].name.c_str(), -1);
-	}
+	// stats.AddRel(rel, 100);
+	// for(int i = 0; i < s->GetNumAtts(); ++i) {
+	// 	stats.AddAtt(rel, s->GetAtts()[i].name.c_str(), -1);
+	// }
 }
 
 
@@ -21,6 +21,7 @@ int main(int argc, char const *argv[]) {
 	char* catalogFName = "data/catalog";	
 	string dbLocation = "data/DB/10M/";
 	Statistics stats = Statistics();
+	stats.Read("stats.txt");
 	RelationData relations = RelationData();
 	char* rels[] = {"supplier", "nation", "region", "customer", "part", "partsupp", "orders", "lineitem"};
 
